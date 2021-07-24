13d4e12ecfe1adaa24571f581bc1ceb775369058
SaravanaKumaran M <saravanankmk17@gmail.com>
2021-07-24 18:01:12 +0530

    added comments

--- compete_cell.py
+++ compete_cell.py
@@ -1,13 +1,17 @@
 #active cells and inactive cells.
-arr = list(map(int, input().split()))
+#do refer this link if you have any doubts in question
+#https://www.geeksforgeeks.org/active-inactive-cells-k-days/
+
+
+arr = list(map(int, input().split())) # getting array from user
 n = len(arr)
-days = int(input())
-for day in range(days):
+k_days = int(input())
+for day in range(k_days):
     for i in range(n):
-        if i==0:
+        if i==0:    # for the first cell which has only one neighbour
             ps = arr[i]
             arr[i] = 0 ^ arr[i+1]
-        elif i==n-1:
+        elif i==n-1: # for the last cell which too has only one neigbour
             arr[i] = ps^0
         else:
             ps1 = arr[i]
